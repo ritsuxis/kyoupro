1b7909c2815e0ae6786e491585351c3b13d21c30
seiritsu <ritsuxis@gmail.com>
2020-02-16 02:34:11 +0900

    I solved abc 004, 010, 029, 033, 039, 041c

---
+++ abc001-020/abc004c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n ; i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x) // decltypeで型取得、引数があればva_argsのところに入れる
+using namespace std;
+typedef long long ll; // long longをllでかけるようにした
+const int INF = 1e9;
+const double PI = 3.1415926535;
+const int MOD = 1000000007; // 計算してからmodで割る ans = (ans * a) % mod
+#define int long long
+template <typename T = long long > T in () { T x; cin >> x; return(x);}
+// int a = in() のように使うlong long以外の型の時はstirng s = in<string>()のように型を指定する
+
+signed main(void){
+    vector<int> card = {1, 2, 3, 4, 5, 6};
+    int n = in();
+    REP(i, n){
+        int tmp = i % 5;
+        swap(card[tmp], card[tmp + 1]);
+    }
+    REP(i, 6) cout << card[i];
+    cout << endl;
+}
