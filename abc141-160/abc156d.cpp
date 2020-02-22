5fa1890bc823e581a0e1fcebcfdcdb94dd84f134
seiritsu <ritsuxis@gmail.com>
2020-02-23 03:21:48 +0900

    I entered abc156

---
+++ abc141-160/abc156d.cpp
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n ; i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define whole(f, x, ...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x) // decltypeで型取得、引数があればva_argsのところに入れる
+using namespace std;
+typedef long long ll; // long longをllでかけるようにした
+const int INF = 1e9;
+const double PI = 3.1415926535;
+const int MOD = 1000000007; // 計算してからmodで割る ans = (ans * a) % mod
+#define int long long
+template <typename T = long long > T in () { T x; cin >> x; return(x);}
+// int a = in() のように使うlong long以外の型の時はstirng s = in<string>()のように型を指定する
+// modint: mod 計算を int を扱うように扱える構造体
+const int MAX = 10e5 * 2 + 10;
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+
+using mint = Fp<MOD>;
+
+int power(int x, int n, int M){
+    int res = 1;
+    if(n > 0){
+        res = power(x, n/ 2, M);
+        if(n % 2 == 0) res = (res * res) % M;
+        else res = (((res * res) % M) * x) % M;
+    }
+    return res;
+}
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+signed main(void){
+    int n, a, b; cin >> n >> a >> b;
+    mint ans = power(2, n, MOD) - 1;
+    
+    /*
+    COMinit();
+    ans -= COM(n, b) + COM(n, a);
+    */
+    mint tmp = 1;
+    REP(i, a){
+        tmp *= (i + 1);
+    }
+    mint p = 1;
+    REP(i, a){
+        p *= (n - i);
+    }
+    ans -= p / tmp;
+
+
+    tmp = 1;
+    REP(i, b){
+        tmp *= (i + 1);
+    }
+    p = 1;
+    REP(i, b){
+        p *= (n - i);
+    }
+    ans -= p / tmp;
+
+    cout << ans << endl;
+
+
+}
